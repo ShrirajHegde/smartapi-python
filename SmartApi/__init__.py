bec9b5852000e945399dc8fab7a33d4269c39c1e
Shriraj Hegde <shriraj.hegde@gmail.com>
2024-01-31 18:17:29 +0530

    Make modules importable without installation Gitignore pyc files

--- SmartApi/__init__.py
+++ SmartApi/__init__.py
@@ -1,11 +1,12 @@
-from __future__ import unicode_literals,absolute_import
+from __future__ import unicode_literals, absolute_import
+import sys, os
+
+module_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+sys.path.insert(0, module_path)
 
 from SmartApi.smartConnect import SmartConnect
+
 # from SmartApi.webSocket import WebSocket
 from SmartApi.smartApiWebsocket import SmartWebSocket
 
-__all__ = ["SmartConnect","SmartWebSocket"]
-
-
-
-
+__all__ = ["SmartConnect", "SmartWebSocket"]
